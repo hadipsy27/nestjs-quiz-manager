73da3455559e924724c31c795d30244580b1e977
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-05-24 14:54:58 +0700

    05 Setting up TypeOrm with Nest JS and create Entity + posgreSQL with docker

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5434,
+  username: 'postgres',
+  password: '123',
+  database: 'quiz',
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true, // Be careful with this option and don't use this in production - otherwise you can lose production data. 
+}
