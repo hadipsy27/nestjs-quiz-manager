b131c6c30eec50e335b80b5b26572aebd2e430b5
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-05-28 17:15:58 +0700

    14 Nestjs Config module to work with env files and hide your secrets from codebase

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,29 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5434,
-  username: 'postgres',
-  password: '123',
-  database: 'quiz',
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true, // Be careful with this option and don't use this in production - otherwise you can lose production data.
-  logging: true,
+export default class TypeOrmConfig {
+  static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_NAME'),
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: true, // Be careful with this option and don't use this in production - otherwise you can lose production data.
+      logging: true,
+    };
+  }
+}
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
+  inject: [ConfigService],
 };
