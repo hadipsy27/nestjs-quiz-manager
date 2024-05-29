d522d987d4f8c9827eade4a10a883350bd7dfbe0
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-05-29 11:58:34 +0700

    update user Registration hash password beforeInsert in entity

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,18 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserRegisterDtoReq } from './dto/user-register-dto.req';
 import { User } from './user.entity';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   async register(userRegisterReq: UserRegisterDtoReq): Promise<User> {
-    const salt = await bcrypt.genSalt();
-    const password = await bcrypt.hash(userRegisterReq.password, salt);
-
     const user = new User();
     user.name = userRegisterReq.name;
     user.email = userRegisterReq.email;
-    user.password = password;
+    user.password = userRegisterReq.password;
 
     return await user.save();
   }
