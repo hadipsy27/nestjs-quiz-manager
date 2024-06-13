6ef6e5045d962f60549f09bb7dc59cfb94a7feea
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-06-13 17:23:03 +0700

    Seeder with typeOrm

---
+++ src/database/seeders/seeders.ts
@@ -0,0 +1,28 @@
+import { User } from '../../modules/user/user.entity';
+import { connectionSource } from '../../config/typeorm.config';
+
+connectionSource
+  .initialize()
+  .then(async (connection) => {
+    const userRepository = connection.getRepository(User);
+
+    // Jalankan query SQL langsung
+    const users = await userRepository.query('SELECT * FROM users');
+    console.info(users);
+
+    // Tambahkan data seeding jika perlu
+    const user = new User();
+    user.name = 'Jojhn Doel';
+    user.email = 'jojhn.doel@example.com';
+    user.password = 'Password@123';
+    user.createAt = new Date(Date.now());
+    user.updatedAt = new Date(Date.now());
+    await userRepository.save(user);
+
+    console.log('User has been saved');
+
+    await connection.destroy();
+  })
+  .catch((error) => console.log(error));
+
+// npm run seeder:run
