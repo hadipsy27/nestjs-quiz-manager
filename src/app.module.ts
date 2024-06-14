136c6401b0a73363addecc67d09e29eec8c11962
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2024-06-14 09:28:33 +0700

    23 Nest JS How to use middleware in Nest JS and it's concept

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,9 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,6 +12,7 @@ import typeorm from './config/typeorm.config';
 import { QuizModule } from './modules/quiz/quiz.module';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ApiTokenCheckMiddleware } from './middleware/api-token-check.middleware';
 
 @Module({
   imports: [
@@ -26,4 +32,10 @@ import { AuthModule } from './modules/auth/auth.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(ApiTokenCheckMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
